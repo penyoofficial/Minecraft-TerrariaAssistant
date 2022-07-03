814ce0506b46a25fb2cb1b5754f816bd2426ab50
Penyo <93194508+penyoofficial@users.noreply.github.com>
2022-07-03 19:50:32 +0800

    Add files via upload

---
+++ src/com/penyo/terrariacoloredtaggenerator/Main.java
@@ -0,0 +1,19 @@
+package com.penyo.terrariacoloredtaggenerator;
+
+/**
+ * “你感到有个邪恶的东西在看着你……”
+ * 这是来自于Terraria系统的独特提示！
+ * 
+ * <p>
+ * 绿色，代表着事件；紫色，代表着战斗；红色，代表着死亡......
+ * 配合游戏中的广播盒，你能使用 {@code TCTG} 模拟出独特的效果！
+ * 恶搞好友什么的，你难以抗拒此诱惑！
+ * 为了使你的用语更逼真， {@code TCTG} 还贴心的准备了模板以供使用——你只需要改改里面的名字！
+ * 
+ * @author Penyo
+ */
+public class Main {
+    public static void main(String[] arg) {
+        new GUI();
+    }
+}
